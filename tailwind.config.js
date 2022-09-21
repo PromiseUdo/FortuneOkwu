1114127330e8cfe6fe1fd203e623a4872978a59a
Promise Udo <info.promiseudo@gmail.com>
2022-09-21 07:05:20 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: ['./*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: 'rgb(10, 25, 47)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: 'hsl(219, 30%, 18%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(0, 100%, 63%)',
+        primary: 'rgb(100, 255, 218)',
+        grayTextColor: 'rgb(136, 146, 176)',
+        grayHeadingColor: 'rgb(204, 214, 246)',
+        projectDescBgColor: 'rgb(17, 34, 64)',
+      },
+      fontFamily: {
+        sans: ['Raleway', 'sans-serif'],
+        opensans: ['Source Code Pro', 'monospace'],
+      },
+      backgroundImage: (theme) => ({
+        'logo-dark-mode': "url('../images/logo.png')",
+        'logo-light-mode': "url('../images/logo-light-mode.svg')",
+        'curvy-dark-mode': "url('../images/bg-curvy-dark-mode.svg')",
+        'curvy-light-mode': "url('../images/bg-curvy-light-mode.svg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundImage: ['dark'],
+    },
+  },
+  plugins: [],
+}
