0dabbfba15662ff2711594e825ab3a55c0da778e
PromiseUdo <info.promiseudo@gmail.com>
2023-05-17 10:42:25 +0100

    verceljson

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const app = require("express")();
 const { v4 } = require("uuid");
 
+const express = require("express");
+
 app.get("/api", (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader("Content-Type", "text/html");
@@ -11,6 +13,27 @@ app.get("/api", (req, res) => {
 app.get("/api/item/:slug", (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
+});
+
+const PORT = process.env.PORT || 3001;
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.render("index.ejs");
+});
+
+app.get("/posts", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.render("posts.ejs");
+});
+app.listen(PORT, () => {
+  console.log("App started on port 3001");
 });
 
 module.exports = app;
