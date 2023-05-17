15c9e3966b0a83514976ff78522c240dfa4a5b21
PromiseUdo <info.promiseudo@gmail.com>
2023-05-17 10:31:33 +0100

    vercel config file

--- app.js
+++ app.js
@@ -5,17 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.set("view engine", "ejs");
 
-// mongoose
-//   .connect('mongodb://127.0.0.1:27017/fitnesblog')
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log('App started on port 3000')
-//     })
-//   })
-//   .catch(() => {
-//     console.log('Database connection failed')
-//   })
-
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
