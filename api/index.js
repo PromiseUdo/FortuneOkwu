83f5fff814257ffb1ff3fb9bbf0b5b5c20cb060f
PromiseUdo <info.promiseudo@gmail.com>
2023-05-17 10:51:33 +0100

    verceljson

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ app.get("/api", (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.end("index.ejs");
 });
 
 app.get("/api/item/:slug", (req, res) => {
@@ -21,13 +21,13 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.end("index.ejs");
 });
 
-app.get("/posts", (req, res) => {
+app.get("/api/posts", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.end("posts.ejs");
